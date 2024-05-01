7484268afecb05fe5686bf944b12e9e8a90cc60f
Akim <akimslys2003@gmail.com>
2024-05-01 23:53:01 +0100

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hltv_async_api',
-    version='0.8.0b1',
+    version='0.8.0',
     author='akimerslys',
     author_email='akimslys2003@gmail.com',
     description='An unofficial asynchronous HLTV API Wrapper for Python\nUse only in non-commercial purposes.',
